45a89fd1344bbe40372b3b5e2134ec8601398bf1
priscelis <prisceliscodrington@gmail.com>
2026-04-04 03:07:54 +0200

    feat: add string reversal exercise

---
+++ level3/StringReversal.java
@@ -0,0 +1,33 @@
+package level3;
+import java.util.Scanner;
+import java.lang.*;
+
+public class StringReversal {
+    private Scanner word;
+    private String usedWord;
+
+    public void storeWord(){
+        word = new Scanner(System.in);
+        System.out.println("Enter a word: ");
+        usedWord = word.next();
+    }
+
+    public void Reverse(){
+        
+        String result = "";
+
+        //  If I don't use length() - 1, it throws StringIndexOutOfBoundsException because indices start at 0, not 1
+        for(int x = usedWord.length()- 1; x >= 0 ; x--){
+            result = result + usedWord.charAt(x);
+        }
+        System.out.println("The reversal word is: "+ result);
+       
+    }
+
+    
+    public static void main(String[] args) {
+        StringReversal string = new StringReversal();
+        string.storeWord();
+        string.Reverse();
+    }
+}
--- prueba/prueba.java
+++ prueba/prueba.java
@@ -1,6 +1,8 @@
 package prueba;
+
 public class prueba   {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola, Hello!");
+        System.out.println("Welcome to my practice space :)");
     }
 }
