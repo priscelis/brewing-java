d8635d49f07078a7eebb1ff8fab5df1c4cbdaf36
priscelis <prisceliscodrington@gmail.com>
2026-04-07 19:18:24 +0200

    wip: start max finder exercise

---
+++ level5/MaxFinder.java
@@ -0,0 +1,21 @@
+package level5;
+import java.util.Random; // Add random numbers of 1 out of 40
+
+/*Given an array of numbers, find the largest number without using .max() or any built-in sorting method.
+
+Example: [3, 7, 1, 9, 4] → 9
+Example: [10, 25, 3, 8] → 25 
+*/
+
+public class MaxFinder {
+    /*
+     What do I need?
+
+     What do I want to do?
+      What are my steps?
+
+    */    
+    public static void main(String[] args) {
+        
+    }
+}
