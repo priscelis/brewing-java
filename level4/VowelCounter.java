8b3d90eb149643f8bd1c712cbb8d2a2a06eb2302
priscelis <prisceliscodrington@gmail.com>
2026-04-07 17:41:47 +0200

    feat: Saving Vocal Counter's excersice

---
+++ level4/VowelCounter.java
@@ -0,0 +1,25 @@
+package level4;
+import java.util.Scanner;
+
+/*
+    Dado un string, cuenta cuántas vocales tiene.
+        Ejemplo: "hello" → 2
+        Ejemplo: "java" → 2
+*/
+
+public class VowelCounter {
+
+    /* 
+    What do I need?
+        1. I need a variable that stores the word I'm writing down. 
+        2. A counter variable, this has to count the amount of vocals. 
+        3. a method so it can find the vocals on the word
+    What do I want to do?
+        I can tryna do like a for 
+    What are my steps?
+
+    */
+    public static void main(String[] args) {
+        
+    }
+}
